a98cf0f8edabdc8845fbca64bbe85b7ab90b5100
Bob Zhang <270489537+bobzhangtech@users.noreply.github.com>
2026-04-06 12:11:54 -0700

    Fix favicon issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/test-site/',
+  base: '/personal-portfolio/',
   plugins: [react(), tailwindcss()],
 })
